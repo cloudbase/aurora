1b929b7e8f76d4c5b8e768bf4e00ed776188f1e9
George Vrancianu <gvrancianu@cloudbasesolutions.com>
2017-02-22 12:26:18 +0200

    Fixes watch error on linux terminals

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function (grunt) {
       },
       livereload: {
         options: {
-          open: true,
+          open: false,
           middleware: function (connect) {
             return [
               serveStatic('.tmp'),
